1df77679a8c2183b7777329932988c5175f9e67c
EusebioId360 <eusebio@id360.com.br>
2022-04-04 00:40:48 -0300

    Pequena mudança corrigida

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { ReactComponent as GithubIcon } from '../../assets/github.svg';
-import { GithubLink, HeaderContainer, Title, HeaderIconsContainer } from './styled';
+import { HeaderContainer, Title, HeaderIconsContainer } from './styled';
 import { useDispatch, useSelector } from 'react-redux';
 import DarkModeToggle from 'react-dark-mode-toggle';
 import { AppStore } from '../../store/store';
@@ -14,9 +13,6 @@ const Header: React.FC = () => {
     <HeaderContainer>
       <HeaderIconsContainer>
         <DarkModeToggle checked={isDarkMode} onChange={() => dispatch(toggleDarkMode())} size={60} />
-        <GithubLink href="https://github.com/eusebiomae" target='_blank'>
-          <GithubIcon />
-        </GithubLink>
       </HeaderIconsContainer> <br />
       <Title>Weather_WIT</Title>
     </HeaderContainer>
